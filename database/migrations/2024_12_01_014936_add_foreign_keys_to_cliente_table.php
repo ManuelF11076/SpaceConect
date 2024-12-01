5e20ea71bc43abce203f4c3fc18651c41732f5fe
ManuelF11076 <mfuentesr14@miumg.edu.gt>
2024-11-30 19:53:41 -0600

    creacion de migraciones

---
+++ database/migrations/2024_12_01_014936_add_foreign_keys_to_cliente_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cliente', function (Blueprint $table) {
+            $table->foreign(['direcciones_id'], 'fk_cliente_direcciones')->references(['id'])->on('direcciones')->onUpdate('no action')->onDelete('no action');
+            $table->foreign(['pagos_id'], 'fk_cliente_pagos1')->references(['id'])->on('pagos')->onUpdate('no action')->onDelete('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cliente', function (Blueprint $table) {
+            $table->dropForeign('fk_cliente_direcciones');
+            $table->dropForeign('fk_cliente_pagos1');
+        });
+    }
+};
